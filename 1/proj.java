ce790ad9c386f15327f6cd8825d6335623588db6
garipov.roma <devilgar@gmail.com>
2019-11-22 12:53:46 +0300

    Parser sosatb

--- 1/proj.java
+++
@@ -1,8 +0,0 @@
-public class proj{
-    public static void main(String[] args){
-        for (long i = 0; i < 64; i++){
-            long x = (1 << i);
-            System.out.println(x);
-        }
-    }
-}
